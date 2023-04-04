fb7163cedcf6b0cfde937b20ed78d49c8b29112d
Tim Gold <timothygold@comcast.net>
2023-04-04 12:10:54 -0400

    added fetch command

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,28 @@
-var gettickEvent = function () {
+console.log ("hi")
+function gettickEvent() {
+  
     var apiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=1f2AwjK2AAERSzyWIP5MWX9nLRXGFLGZ';
     console.log(apiUrl)
-  
+
     fetch(apiUrl)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-            displayVenues(data);
+        .then(function (response) {
+            return response.json();
+            // if (response.ok) {
+            //response.json()
+            // .then(function (data) {
+            //displayVenues(data);
+            //  console.log(data)
+            // });
+            //} else {
+            //alert('Error: ' + response.statusText);
+            // }//
+        }).then(function (data) {
             console.log(data)
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert('Unable to connect to Ticketmaster');
-      });
-  };
-  apiUrl ()
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
+
+//displayVenues ()
+gettickEvent()
